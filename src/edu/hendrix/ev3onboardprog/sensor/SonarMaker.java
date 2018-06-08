02f4ff87f52f5fc2a610cc4b93f58db82d1894ff
Gabriel Ferrer <ferrer@hendrix.edu>
2018-06-08 08:55:33 -0500

    Eliminated sonarMaker package

--- src/edu/hendrix/ev3onboardprog/sensor/SonarMaker.java
+++
@@ -1,8 +0,0 @@
-package edu.hendrix.ev3onboardprog.sensor;
-
-public class SonarMaker {
-	public static void main(String[] args) {
-		Display disp = new Display();
-		disp.create().execute();
-	}
-}
